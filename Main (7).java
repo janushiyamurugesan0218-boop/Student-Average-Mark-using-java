451db2d87ddb31e52bba740a9373fd4a859d0cba
Janushiya <janushiyamurugesan0218@gmail.com>
2026-07-03 21:51:53 +0530

    Add files via upload

---
+++ Main (7).java
@@ -0,0 +1,13 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	   int subject1=78;
+	   int subject2=84;
+	   int subject3=88;
+	 int sum=subject1+subject2+subject3;
+	 int average=sum/3;
+		System.out.println("Average="+average);
+	}
+}
